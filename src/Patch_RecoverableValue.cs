836af5d818969c3e02697ec430c5088ac3911953
Kerem00 <kerembicen94@gmail.com>
2026-07-09 18:05:54 +0300

    source files

---
+++ src/Patch_RecoverableValue.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using SunnySideUp;
+
+namespace UnlimitedStamina
+{
+    [HarmonyPatch(typeof(RecoverableValue), "CanConsume")]
+    public static class Patch_RecoverableValue_CanConsume
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref bool __result)
+        {
+            __result = true;
+
+            return false;
+        }
+    }
+}
---
+++ src/UnlimitedStaminaMod.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace UnlimitedStamina
+{
+    [BepInPlugin("lwitw.mods.UnlimitedStamina", "Unlimited stamina", "1.0.0")]
+    public class UnlimitedStacksMod : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            new Harmony("lwitw.mods.UnlimitedStamina").PatchAll();
+            this.Logger.LogInfo((object) "Stamina is unlimited");
+        }
+    }
+}
